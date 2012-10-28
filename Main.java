de3c3b40380a63bcfe75168b2100028470261b0c
jonh974 <jonathan.minatchy@gmail.com>
2012-10-28 18:54:56 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+/* Program wich ... */
+
+/**
+	@author Jonathan MINATCHY
+	@version 24/09/2012
+ */
+public class Main {
+	
+	/* Main method */
+	public static void main (String[] args) {
+		System.out.println("Hello World !");
+	}
+}
